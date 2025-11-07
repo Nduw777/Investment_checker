7469df1ac886abf12329acba248641da3bb333f5
Nduw777 <vincentnduwimana2024@gmail.com>
2025-11-07 10:22:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -78,5 +78,5 @@ def index():
 
     return render_template("index.html", result=result)
 
-# if __name__ == "__main__":
-    # app.run(debug=True)
+if __name__ == "__main__":
+    app.run(debug=True)
